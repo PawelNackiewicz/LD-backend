b8d961d2501e376b104a2e6317e6aa9796861f08
pnackiewicz <it.pnackiewicz@gmail.com>
2020-07-29 22:00:24 +0200

    :fire: remove multiple import

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { AppController } from './app.controller';
-import { UsersService } from './users/users.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { FacilityModule } from './facility/facility.module';
 
@@ -14,6 +13,6 @@ import { FacilityModule } from './facility/facility.module';
     FacilityModule,
   ],
   controllers: [AppController],
-  providers: [UsersService],
+  providers: [],
 })
 export class AppModule {}
