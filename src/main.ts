c11b09d8476f1b0661e802d8e7107917d7c5ab25
pnackiewicz <it.pnackiewicz@gmail.com>
2020-09-01 21:37:54 +0200

    :fix: change cors

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,13 @@ import { ConfigService } from './config/config.service';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  app.enableCors();
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true
+  });
   const configService = app.get(ConfigService);
   await app.listen(configService.get('PORT'));
 }
+
 bootstrap();
